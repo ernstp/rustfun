84178d8f8b97fbcf52d92a0c83b6b55ad450bf5f
Ernst Sjöstrand <ernstp@gmail.com>
2015-04-19 22:12:30 +0200

    Make it work on Rust 1.0 beta

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::collections::BTreeSet;
-use std::collections::PriorityQueue;
-use std::num::abs;
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;
 use std::rc::Rc;
-use std::rand;
+extern crate rand;
+use rand::random;
 
-#[deriving(Ord,Eq)]
+#[derive(Ord,Eq)]
 struct Point {
-    x: uint,
-    y: uint,
-    path: uint,
-    cost: uint,
+    x: u32,
+    y: u32,
+    path: u32,
+    cost: u32,
     parent: Option<Rc<Point>>,
 }
 
 impl Point {
-    fn get_dist(&self, other: &Point) -> uint {
-        (abs(self.x as int - other.x as int) + abs(self.y as int - other.y as int)) as uint
+    fn get_dist(&self, other: &Point) -> u32 {
+        ((self.x as i32 - other.x as i32).abs() + (self.y as i32 - other.y as i32).abs()) as u32
     }
 
     fn print_path(&self) {
@@ -25,7 +26,7 @@ impl Point {
         println!("{},{}", self.x, self.y);
     }
 
-    fn new(x: uint, y: uint, inparent: Option<&Rc<Point>>, target: &Point) -> Point {
+    fn new(x: u32, y: u32, inparent: Option<&Rc<Point>>, target: &Point) -> Point {
         let mut p = Point {x: x,
                y: y,
                path: 0,
@@ -52,9 +53,9 @@ impl PartialOrd for Point {
 }
 
 struct Map {
-    data: Vec<Vec<uint>>,
-    sizex: uint,
-    sizey: uint,
+    data: Vec<Vec<u32>>,
+    sizex: u32,
+    sizey: u32,
 }
 
 impl Map {
@@ -67,27 +68,27 @@ impl Map {
         }
     }
 
-    fn index(&self, x: uint, y: uint) -> uint {
+    fn index(&self, x: u32, y: u32) -> u32 {
         y * self.sizex + x
     }
 
-    fn avail(&self, x: uint, y: uint) -> bool {
+    fn avail(&self, x: u32, y: u32) -> bool {
         if x <= self.sizex && y <= self.sizey {
-            self.data[y][x] == 0
+            self.data[y as usize][x as usize] == 0
         } else {
             false
         }
     }
 
-    fn new(sizex: uint, sizey: uint) -> Map {
-        let mut d = Vec::with_capacity(sizey);
-        for row in range(0, sizey) {
-            let mut dr = Vec::with_capacity(sizex);
-            for x in range(0, sizex) {
-                let r = rand::random::<uint>() % 3u;
+    fn new(sizex: u32, sizey: u32) -> Map {
+        let mut d = Vec::with_capacity(sizey as usize);
+        for row in (0..sizey) {
+            let mut dr = Vec::with_capacity(sizex as usize);
+            for x in (0..sizex) {
+                let r = rand::random::<u32>() % 3;
                 match r {
-                    0 => dr.push(1u),
-                    _ => dr.push(0u),
+                    0 => dr.push(1),
+                    _ => dr.push(0),
                 }
             }
             d.push(dr);
@@ -97,12 +98,12 @@ impl Map {
 }
 
 fn main() {
-    let adjecent = vec![(1i,1i), (1i,0i), (1i,-1i), (0i,-1i), (-1i,-1i), (-1i,0i), (-1i,1i), (0i,1i)];
+    let adjecent = vec![(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)];
 
     let map = Map::new(10, 3);
     map.print();
     let mut used = BTreeSet::new();
-    let mut openq = PriorityQueue::new();
+    let mut openq = BinaryHeap::new();
     let start = Point {x: 0, y: 0, path: 0, cost: 0, parent: None};
     let target = Point {x: 9, y: 2, path: 0, cost: 0, parent: None};
     used.insert(map.index(start.x, start.y));
@@ -113,11 +114,11 @@ fn main() {
 
     while *current != target {
         for &(x, y) in adjecent.iter() {
-            let newx: int = current.x as int + x as int;
-            let newy: int = current.y as int + y as int;
-            if newy >= 0 && newx >= 0 && newx < map.sizex as int && newy < map.sizey as int {
-                let newx: uint = newx as uint;
-                let newy: uint = newy as uint;
+            let newx: i32 = current.x as i32 + x as i32;
+            let newy: i32 = current.y as i32 + y as i32;
+            if newy >= 0 && newx >= 0 && newx < map.sizex as i32 && newy < map.sizey as i32 {
+                let newx: u32 = newx as u32;
+                let newy: u32 = newy as u32;
                 let index = map.index(newx, newy);
                 // TODO: If the point exists in the openq and this is a shorter
                 // path to this point it should be updated.
